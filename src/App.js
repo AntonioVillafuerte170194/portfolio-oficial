e6093482166353072765fa504c1996e303f83a35
AntonioVillafuerte170194 <villafuerteantonio74@gmail.com>
2023-05-02 13:06:53 -0300

    added auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { Home } from "./components/Home";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 import { WebsiteList } from "./components/WebsiteList";
 import { WebsiteForm } from "./components/WebsiteForm";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<WebsiteList />} />
-          <Route path="add" element={<WebsiteForm />} />
-          <Route path="edit/:id" element={<WebsiteForm />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="bg-slate-300 text-black h-screen flex text-white">
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ProtectedRoute><Home/><WebsiteList/></ProtectedRoute>}/>
+          <Route path="add" element={<ProtectedRoute><Home/><WebsiteForm /></ProtectedRoute>} />
+          <Route path="edit/:id" element={<ProtectedRoute><Home/><WebsiteForm /></ProtectedRoute>} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthProvider>
+    </div>
   );
 }
 
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,22 @@
-import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  // Paste Your keys here
+const firebaseConfig = {
+  apiKey: "AIzaSyDNDT_s-X5lciC2RM2tPfZTmsBEaPlzHYQ",
+  authDomain: "portfolio-crud-b38d0.firebaseapp.com",
+  projectId: "portfolio-crud-b38d0",
+  storageBucket: "portfolio-crud-b38d0.appspot.com",
+  messagingSenderId: "30998119040",
+  appId: "1:30998119040:web:ec8f57970a047dc0145c65"
 };
-// Initialize Firebase
-initializeApp(firebaseConfig);
 
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
+export const auth = getAuth(app);
